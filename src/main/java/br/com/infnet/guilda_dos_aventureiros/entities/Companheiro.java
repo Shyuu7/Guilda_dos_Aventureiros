26920c589962f49ad97d8362c4cab598ee0f7ed6
Larissa Conti <laricbarros@gmail.com>
2026-03-22 14:23:42 -0300

    mapear companheiro para o banco de dados

--- src/main/java/br/com/infnet/guilda_dos_aventureiros/entities/Companheiro.java
+++ src/main/java/br/com/infnet/guilda_dos_aventureiros/entities/Companheiro.java
@@ -1,19 +1,54 @@
 package br.com.infnet.guilda_dos_aventureiros.entities;
 
 import br.com.infnet.guilda_dos_aventureiros.enums.CompanheiroEspecies;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+@Table(name = "companheiros", schema = "aventura",
+        indexes = {
+            @Index(name = "idx_companheiro_especie", columnList = "especie, lealdade")
+        })
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Companheiro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
     private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "especie", nullable = false)
     private CompanheiroEspecies especie;
+
+    @Range(min = 0, max = 100, message = "Lealdade deve ser um número inteiro entre 0 e 100")
+    @Column(name = "lealdade", nullable = false)
     private int lealdade;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @PrimaryKeyJoinColumn(name = "aventureiro_id", foreignKey = @ForeignKey(name = "fk_companheiro_aventureiro"))
+    private Aventureiro aventureiro;
 
     public Companheiro(String nome, CompanheiroEspecies especie, int lealdade) {
         this.nome = nome;
         this.especie = especie;
         this.lealdade = lealdade;
+    }
+
+    void associarAventureiro(Aventureiro aventureiro) {
+        this.aventureiro = aventureiro;
+        aventureiro.setCompanheiro(this);
+    }
+
+    void removerAventureiro() {
+        this.aventureiro = null;
     }
 }
