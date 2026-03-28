57846b927ea63f35680eacf1283bffe16f24e632
Larissa Conti <laricbarros@gmail.com>
2026-03-28 15:54:55 -0300

    criar novos repositories para conexão com o banco de dados

---
+++ src/main/java/br/com/infnet/guilda_dos_aventureiros/repositories/aventura/AventureiroRepository.java
@@ -0,0 +1,29 @@
+package br.com.infnet.guilda_dos_aventureiros.repositories.aventura;
+
+import br.com.infnet.guilda_dos_aventureiros.dto.aventura.AventureiroResumoResponse;
+import br.com.infnet.guilda_dos_aventureiros.entities.aventura.Aventureiro;
+import br.com.infnet.guilda_dos_aventureiros.enums.aventura.AventureiroClasses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface AventureiroRepository extends JpaRepository<Aventureiro, Long> {
+    @Query(value = """
+            SELECT new br.com.infnet.guilda_dos_aventureiros.dto.aventura.AventureiroResumoResponse
+                        (a.id, a.nome, a.classe, a.nivel, a.ativo)
+            FROM Aventureiro a
+            WHERE (:status IS NULL OR a.ativo = :status) AND
+            (:pClasse IS NULL OR a.classe = :pClasse) AND
+            (:nivelMinimo IS NULL OR a.nivel >= :nivelMinimo)
+            """)
+    Page<AventureiroResumoResponse> findByStatusAndClasseAndNivelMinimo(
+            @Param("status") Boolean status,
+            @Param("pClasse") AventureiroClasses classe,
+            @Param("nivelMinimo") Integer nivelMinimo,
+            Pageable pageable
+    );
+
+}
