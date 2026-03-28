42ba991e7952fe74cf8225dfebde318102d6a0cf
Larissa Conti <laricbarros@gmail.com>
2026-03-28 19:38:03 -0300

    criar novo DTO para response de Missões

---
+++ src/main/java/br/com/infnet/guilda_dos_aventureiros/dto/aventura/MissaoResponse.java
@@ -0,0 +1,26 @@
+package br.com.infnet.guilda_dos_aventureiros.dto.aventura;
+
+import br.com.infnet.guilda_dos_aventureiros.enums.aventura.NivelPerigo;
+import br.com.infnet.guilda_dos_aventureiros.enums.aventura.StatusMissao;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class MissaoResponse {
+    @NotNull
+    private Long id;
+    @Length(min = 1, max = 150)
+    private String titulo;
+    @NotNull
+    private NivelPerigo nivelPerigo;
+    @NotNull
+    private StatusMissao status;
+    private LocalDateTime dataInicio;
+    private LocalDateTime dataTermino;
+    private Long organizacaoId;
+    private List<ParticipacaoResponse> participacoes;
+}
