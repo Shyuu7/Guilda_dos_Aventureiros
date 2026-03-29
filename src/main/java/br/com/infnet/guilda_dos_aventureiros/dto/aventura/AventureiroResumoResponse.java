64d75c4edb3d473155a056720a252cba40c96530
Larissa Conti <laricbarros@gmail.com>
2026-03-29 13:29:42 -0300

    adicionar validaçao no dto de resposta resumida do aventureiro

--- src/main/java/br/com/infnet/guilda_dos_aventureiros/dto/aventura/AventureiroResumoResponse.java
+++ src/main/java/br/com/infnet/guilda_dos_aventureiros/dto/aventura/AventureiroResumoResponse.java
@@ -1,12 +1,16 @@
 package br.com.infnet.guilda_dos_aventureiros.dto.aventura;
 
 import br.com.infnet.guilda_dos_aventureiros.enums.aventura.AventureiroClasses;
+import jakarta.validation.constraints.NotNull;
 
 public record AventureiroResumoResponse(
+        @NotNull
         Long id,
         String nome,
+        @NotNull
         AventureiroClasses classe,
         int nivel,
+        @NotNull
         boolean ativo
 ) {
 }
