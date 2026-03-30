d25898cb84f4625d99de462ceb98ed617dc53dd1
Larissa Conti <laricbarros@gmail.com>
2026-03-29 23:54:54 -0300

    mudar tipo de recompensa: de Double para BigDecimal

--- src/main/java/br/com/infnet/guilda_dos_aventureiros/dto/relatorios/RankingProjection.java
+++ src/main/java/br/com/infnet/guilda_dos_aventureiros/dto/relatorios/RankingProjection.java
@@ -2,10 +2,12 @@ package br.com.infnet.guilda_dos_aventureiros.dto.relatorios;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import java.math.BigDecimal;
+
 @JsonPropertyOrder({"nomeAventureiro", "totalParticipacoes", "totalRecompensas", "totalDestaques"})
 public interface RankingProjection {
     String getNomeAventureiro();
     Long getTotalParticipacoes();
-    Double getTotalRecompensas();
+    BigDecimal getTotalRecompensas();
     Long getTotalDestaques();
 }
