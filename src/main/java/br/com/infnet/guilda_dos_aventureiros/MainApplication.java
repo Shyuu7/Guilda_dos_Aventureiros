8f83f241ca464ac7f548d3789ce4297296775a13
Larissa Conti <laricbarros@gmail.com>
2026-04-06 21:41:21 -0300

    implementar cache na aplicação principal

--- src/main/java/br/com/infnet/guilda_dos_aventureiros/MainApplication.java
+++ src/main/java/br/com/infnet/guilda_dos_aventureiros/MainApplication.java
@@ -2,11 +2,12 @@ package br.com.infnet.guilda_dos_aventureiros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MainApplication {
 	void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
-
 }
